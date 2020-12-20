887658e961d04dcec020c8b573f81d863c5204be
BartekR <bartek@rudnicki.szczecin.pl>
2020-12-20 13:07:05 +0100

    init

--- data_read.h
+++ data_read.h
@@ -4,5 +4,13 @@
 
 #ifndef RX_BASE_DATA_READ_H
 #define RX_BASE_DATA_READ_H
+#include <stdint.h>
+typedef struct iq_chunk{
+    double *I;      // I part of IQ signal
+    double *Q;      // Q part of IQ signal
+    uint32_t size;  // length of I
+
+}chunk;
+
 
 #endif //RX_BASE_DATA_READ_H
